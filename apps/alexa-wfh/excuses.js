3319b6d664b1351e367c1902c938626956ab3fcb
Ubuntu-Alexa <jason@jayjustice.asuscomm.com>
2017-12-11 21:47:12 -0800

    added files

---
+++ apps/alexa-wfh/excuses.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+    "en-US": {
+        "translation": {
+            "EXCUSES": [
+                "Doctors appointment.",
+                "Sick Kid.",
+                "Food Poisoning.",
+                "Dentist appointment.",
+                "Just spent the past couple of hours in the bathroom, don't ask.",
+                "I'm having furniture delivered to my house.",
+                "I have to sign for a package.",
+                "A pipe burst this morning, waiting on a plumber.",
+                "The package never came, so I have to wait for it again.",
+                "Painted the house and it's not dry yet.",
+                "Taking a friend to the airport.",
+                "I'm having and allergic reaction.",
+                "I lost my car keys.",
+                "I am drowsy from medication.",
+                "I am coming down with a cold.",
+                "I logged in from home this morning and got caught up in work, might as well stay here.",
+                "I have an ingrown toe nail",
+                "I ate some bad tacos last night.",
+                "I have a contractor arriving for home repairs.",
+                "Alexa couldn't find me a good excuse",
+                "I locked my keys in the car, and I'm waiting on a locksmith.",
+                "My electric car didn't charge last night.",
+                "I have lady problems.",
+                "I have a horrible migraine.",
+                "I need to pick up the car from the shop.",
+                "I almost made it to work when the brakes went out. Now I'm waiting on a tow.",
+                "I was working really late last night.",
+            ],
+            "SKILL_NAME" : "Work From Home Excuses",
+            "GET_EXCUSE_MESSAGE" : "Here's your Excuse: ",
+            "HELP_MESSAGE" : "You can say, give me an Excuse, or I'm feeling lazy, or you can say exit... What can I help you with?",
+            "HELP_REPROMPT" : "What can I help you with?",
+            "STOP_MESSAGE" : "Goodbye!"
+        }
+    },
+};
